3dc68349d75d89a92a6328e2db11240b3062816e
B V Raju <bvraju.itp@gmail.com>
2018-03-13 17:26:49 +0530

    fourth saving in DB

--- models/uploaddoc.js
+++ models/uploaddoc.js
@@ -10,7 +10,7 @@ const employeeUploadDocSchema = new SchemaDoc({
     type : String
   },
 
-  finalfilename: {
+  filename: {
     type: String
   },
 
--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,7 @@ var Location = require('../models/location.js');
 var UploadedPic = require('../models/uploadpic.js');
 var UploadedDoc = require('../models/uploaddoc.js');
 
-var multer = require('multer');
+//var multer = require('multer');
 var path = require('path');
 
 //for get request of MongoDB
@@ -16,44 +16,44 @@ var path = require('path');
 
 let emailid, phonenumber, filenameprefix, onlyfilename, finalfilenamewithext, filepath;
 
-var storageFile = multer.diskStorage({
+// var storageFile = multer.diskStorage({
+//
+//     destination: (req, file, cb) => {
+//       cb(null, 'dataprofiles');
+//     },
+//     //cb: callback function
+//     filename: (req, file, cb) => {
+//       emailid = req.body.email;
+//       phonenumber = req.body.phone;
+//       onlyfilename = req.body.onlyfilename;
+//       var cdate = new Date();
+//       filenameprefix  = emailid + "-" + phonenumber + "-" + cdate.getDate() + "-" + (cdate.getMonth() + 1 ) + "-" + cdate.getFullYear() + "-" + cdate.getHours() + "-" + cdate.getMinutes() + "-" + cdate.getSeconds();
+//       finalfilenamewithext = filenameprefix + "-" + onlyfilename;
+//       cb(null, finalfilenamewithext);
+//     }
+// });
 
-    destination: (req, file, cb) => {
-      cb(null, 'dataprofiles');
-    },
-    //cb: callback function
-    filename: (req, file, cb) => {
-      emailid = req.body.email;
-      phonenumber = req.body.phone;
-      onlyfilename = req.body.onlyfilename;
-      var cdate = new Date();
-      filenameprefix  = emailid + "-" + phonenumber + "-" + cdate.getDate() + "-" + (cdate.getMonth() + 1 ) + "-" + cdate.getFullYear() + "-" + cdate.getHours() + "-" + cdate.getMinutes() + "-" + cdate.getSeconds();
-      finalfilenamewithext = filenameprefix + "-" + onlyfilename;
-      cb(null, finalfilenamewithext);
-    }
-});
-
-var storagePic = multer.diskStorage({
-
-    destination: (req, file, cb) => {
-      cb(null, 'dataproimages');
-    },
-    //cb: callback function
-    filename: (req, file, cb) => {
-      var phonenumber = req.body.phone;
-      var emailid = req.body.email;
-      var cdate = new Date();
-      filepath  = emailid + "-" + phonenumber + "-" + cdate.getDate() + "-" + (cdate.getMonth() + 1 ) + "-" + cdate.getFullYear() + "-" + cdate.getHours() + "-" + cdate.getMinutes() + "-" + cdate.getSeconds() + ".jpg";
-
-      cb(null, filepath);
-    }
-});
+// var storagePic = multer.diskStorage({
+//
+//     destination: (req, file, cb) => {
+//       cb(null, 'dataproimages');
+//     },
+//     //cb: callback function
+//     filename: (req, file, cb) => {
+//       var phonenumber = req.body.phone;
+//       var emailid = req.body.email;
+//       var cdate = new Date();
+//       filepath  = emailid + "-" + phonenumber + "-" + cdate.getDate() + "-" + (cdate.getMonth() + 1 ) + "-" + cdate.getFullYear() + "-" + cdate.getHours() + "-" + cdate.getMinutes() + "-" + cdate.getSeconds() + ".jpg";
+//
+//       cb(null, filepath);
+//     }
+// });
 
 //SETTING UP ROUTES - that is to define the application end points (URIs) and set up how the application responds to client requests.
 
 
 router.get('/executives', function(req, res){
-  res.send('Hai Sai Srinivas Sir, this is Datapro Executive Application!');
+  res.send('Hai Sai Srinivas Sir, this is Datapro Executive Application version2!');
 });  //end of get route
 
 //to add a new employee Document in the MongoDB database
@@ -77,7 +77,6 @@ router.post('/executives', function(req, res){
         console.log('Registration has been successful!');
         res.json({"serverresult" : "Successfully Registered!"});
       }
-
    });
 });
 
@@ -121,78 +120,93 @@ router.post('/myuploadedpic', function(req, res){
    // });
 
    //FOR WRITING THE FULL-SIZE PICTURE
-   var upload2 = multer({
-     storage: storagePic
-   }).single('image')
-   upload2(req, res, function(err) {
-
-     if(err){
-       console.log('Image uploading failed' + err);
-       res.send("Image uploading failed" + err);
-     }
-     else{
-       console.log('Image is saved to a file in Server');
+   // var upload2 = multer({
+   //   storage: storagePic
+   // }).single('image')
+   // upload2(req, res, function(err) {
+   //
+   //   if(err){
+   //     console.log('Image uploading failed' + err);
+   //     res.send("Image uploading failed" + err);
+   //   }
+   //   else{
+   //     console.log('Image is saved to a file in Server');
        //now writing the same to MongoDB database
 
-       var uploadedPic = new UploadedPic({
-         email : req.body.email,
-         phone   : req.body.phone,
-         photoDescription : req.body.imageDescription,
-         picDate : req.body.currentDate,
-         picTime : req.body.currentTime   //date and time separated for ease of Analytics and delete data by from and to date
-       });
+       // var uploadedPic = new UploadedPic({
+       //   email : req.body.email,
+       //   phone   : req.body.phone,
+       //   photoDescription : req.body.imageDescription,
+       //   picDate : req.body.currentDate,
+       //   picTime : req.body.currentTime   //date and time separated for ease of Analytics and delete data by from and to date
+       // });
 
-       //Saving the data in the MongoDB Database
+       //Saving the picture details in the MongoDB Database
+       console.log(req.body);
+
+        var uploadedPic = new UploadedPic({
+          email : req.body.email,
+          phone   : req.body.phone,
+          picDate : req.body.currentDate,
+          picTime : req.body.currentTime,
+          photoDescription :   req.body.photoDescription
+        });
+
        uploadedPic.save(function(err){
           if(err){
             console.log('Image details could not be saved to database');
             res.json({"serverpicresult" : "Failed Saving the Pic in the Database!"});
           }else{
             console.log('Image details saved to database');
-            res.json({"serverpicresult" : "Successfully Saved the Pic in the Database!"});
+            res.json({"serverpicresult" : "Pic saved in the Database Successfully!"});
             }
        });
-
-
-     }
-   })
-});
+});  //end of route
 
 router.post('/myuploadeddoc', function(req, res){
 
     //Then saving in a file
-      var upload = multer({
-    		storage: storageFile
-    	}).single('uploaded_file')
-    	upload(req, res, function(err) {
+      // var upload = multer({
+    	// 	storage: storageFile
+    	// }).single('uploaded_file')
+    	// upload(req, res, function(err) {
+      //
+      //   if(err){
+      //     console.log('File uploading failed' + err);
+      //     res.send("File uploading failed" + err);
+      //   }
+        // else{
+        //   console.log('File is uploaded');
+        //
+        //   var uploadedDoc = new UploadedDoc({
+        //     email : req.body.email,
+        //     phone : req.body.phone,
+        //     finalfilename : req.body.onlyfilename,
+        //     docDate : req.body.currentDate,
+        //     docTime : req.body.currentTime   //date and time separated for ease of Analytics and delete data by from and to date
+        //   });
+          //Saving the document data in the MongoDB Database
 
-        if(err){
-          console.log('File uploading failed' + err);
-          res.send("File uploading failed" + err);
-        }
-        else{
-          console.log('File is uploaded');
+          console.log(req.body);
 
-          var uploadedDoc = new UploadedDoc({
-            email : req.body.email,
-            phone : req.body.phone,
-            finalfilename : req.body.onlyfilename,
-            docDate : req.body.currentDate,
-            docTime : req.body.currentTime   //date and time separated for ease of Analytics and delete data by from and to date
-          });
-          //Saving the data in the MongoDB Database
+           var uploadedDoc = new UploadedDoc({
+             email : req.body.email,
+             phone   : req.body.phone,
+             docDate : req.body.currentDate,
+             docTime : req.body.currentTime,
+             finalfilename :   req.body.fileName
+           });
+
           uploadedDoc.save(function(err){
              if(err){
+               console.log('Failed saving File details in the Database!');
                res.json({"serverdocresult" : "failed in saving doc"});
-               console.log('Failed saving File details in the Database');
              }else{
+               console.log('File details saved in the Database Successfully!');
                res.json({"serverdocresult" : "successful in saving doc"});
-               console.log('File details Successfully saved in the Database');
                }
           });
-        }
-    	})
-});
+});  //end of route
 
 //now exporting the above route to be used in the main index.js file
 module.exports = router;
